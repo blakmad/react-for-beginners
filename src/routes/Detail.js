226ca363a851e260b098f49f443dcccd0faa0370
blackmad <kth4865@gmail.com>
2023-02-04 20:45:52 +0900

    detail 추가

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,20 +1,44 @@
 import { useParams } from "react-router-dom";
-import { useEffect } from "react";
-
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 function Detail() {
   const { id } = useParams();
+  const [currMovie, setCurrMovie] = useState("");
+  const [loading, setLoading] = useState(true);
 
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-    console.log(json);
+    setCurrMovie(json);
+    setLoading(false);
   };
-
+  console.log(currMovie);
   useEffect(() => {
     getMovie();
-  });
-  return <h1>Detailtqqqqqqqqqqq</h1>;
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading,,,</h1> // if it is still on loading,,
+      ) : (
+        // if loading finished,,,
+        <div>
+          <img src={currMovie.data.movie.medium_cover_image}></img>
+          <h3> Title : {currMovie.data.movie.title}</h3>
+          <h3> Genres : {currMovie.data.movie.genres[0]}</h3>
+          <h3> Year : {currMovie.data.movie.year}</h3>
+          <h3> Languages : {currMovie.data.movie.language}</h3>
+          <h3>
+            <Link to="/" style={{ textDecoration: "none", color: "black" }}>
+              Go back
+            </Link>
+          </h3>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Detail;
